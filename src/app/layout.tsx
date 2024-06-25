10b21eb24be13950f122a7357c60fee582092f70
bajrayejoon <binaya.bajrashakya@gmail.com>
2024-06-25 16:53:44 +0545

    chore: Update font import & Vercel Analytics for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Paw Prints - Your donation can save a life",
@@ -40,7 +41,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>
+        {children}
+
+        <Analytics />
+      </body>
     </html>
   );
 }
